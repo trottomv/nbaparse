4eeb70f4317b88dd6fef0fdab32e96da8e26cecc
Matteo Vitali <trottomv@gmail.com>
2017-10-21 13:29:46 +0200

    nginx puma sinatra

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'nokogiri'
 gem 'dotenv', '~> 2.1', '>= 2.1.1'
+gem "puma"
--- parsefeed.rb
+++ parsefeed.rb
@@ -4,6 +4,11 @@ require 'open-uri'
 require 'sinatra'
 require 'sinatra/reloader'
 
+configure {
+  set :server, :puma
+}
+
+
 class Teamparse
 
   def initialize
@@ -26,67 +31,22 @@ class Teamparse
     items.css('category').first.text
   end
 
-  # def eachitems
-  #   teamparse.css('item').map { |items| items }
-  # end
-  #
-  #
-  # def itemparse
-  #   items.each do | item |
-  #     Nokogiri::XML(item).map { |item| item }
-  #   end
-  # end
-
-  # def itemsinfo
-  #   puts teamcategory.upcase
-  #   items.map.each_with_index do |item, index|
-  #     @itemtitle = item.css('title').text #.map { |title| title.text }
-  #     @itemurl = item.css('link').text #.map { |link| link.text }
-  #     @itemid = item.css('guid').text #.map { |link| link.text }
-  #     itemcontent = item.xpath('//content:encoded', 'content' => "http://purl.org/rss/1.0/modules/content/").text
-  #     @itemframe = Nokogiri::XML(itemcontent).css('iframe').map {|itemsframe| itemsframe.attr('src') } #.map { |itemframe| itemframe }
-  #
-  #     # puts @itemframe[index]
-  #     puts "#{@itemtitle}
-  #           #{@itemurl}
-  #           #{@itemid}
-  #           #{@itemframe[index]}"
-  #   end
-  # end
-  #
-  # def itemtitle
-  #   items.css('title').map { |title| title.text }
-  # end
-  #
-  # def itemurl
-  #   items.css('link').map { |link| link.text }
-  # end
-  #
-  # def itemid
-  #   items.css('guid').map { |link| link.text }
-  # end
-  #
-  # def itemframe
-  #   itemcontent = items.xpath('//content:encoded', 'content' => "http://purl.org/rss/1.0/modules/content/").text
-  #   Nokogiri::XML(itemcontent).css('iframe').map { |itemframe| itemframe.attr('src') }
-  # end
-
 end
 
-# puts Teamparse.new.itemsinfo
+class Pumatra < Sinatra::Base
+  get '/' do
+    @teamcategory = Teamparse.new.teamcategory.upcase
+    @items = Teamparse.new.items.map.each_with_index do |item, index|
+      @itemcontent = item.xpath('//content:encoded', 'content' => "http://purl.org/rss/1.0/modules/content/").text #.map {|itemcontent| itemcontent.text}
+      {
+        itemtitle: item.css('title').text, #.map { |title| title.text }
+        itemurl: item.css('link').text, #.map { |link| link.text }
+        itemid: item.css('guid').text, #.map { |link| link.text }
+        itemframe: Nokogiri::XML(@itemcontent).css('iframe').map {|itemframe| itemframe.attr('src')}[index] #.map { |itemframe| itemframe }
+      }
+      end
 
-get '/' do
-  @teamcategory = Teamparse.new.teamcategory.upcase
-  @items = Teamparse.new.items.map.each_with_index do |item, index|
-    @itemcontent = item.xpath('//content:encoded', 'content' => "http://purl.org/rss/1.0/modules/content/").text #.map {|itemcontent| itemcontent.text}
-  {
-    itemtitle: item.css('title').text, #.map { |title| title.text }
-    itemurl: item.css('link').text, #.map { |link| link.text }
-    itemid: item.css('guid').text, #.map { |link| link.text }
-    itemframe: Nokogiri::XML(@itemcontent).css('iframe').map {|itemframe| itemframe.attr('src')}[index] #.map { |itemframe| itemframe }
-  }
+    erb :default
+
   end
-
-  erb :default
-
 end
