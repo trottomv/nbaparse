e5590f416c2b8181e999d15a75a7c8c309497b87
trottomv <trottomv@gmail.com>
2018-11-04 08:47:18 +0100

    version 1.1

--- parsefeed.rb
+++ parsefeed.rb
@@ -14,12 +14,24 @@ class Teamparse
     "#{ENV['TEAM_URL']}"
   end
 
+  def allurl
+    "#{ENV['ALL_URL']}"
+  end
+
   def teamparse
     Nokogiri::XML(open("#{teamurl}"))
   end
 
+  def allparse
+    Nokogiri::XML(open("#{allurl}"))
+  end
+
   def items
     teamparse.css('item') #.map { |items| items }
+  end
+
+  def allitems
+    allparse.css('item') #.map { |items| items }
   end
 
   def teamcategory
@@ -33,6 +45,26 @@ configure {
 }
 
 get '/' do
+  #@teamcategory = Teamparse.new.teamcategory.upcase
+  erb :default
+end
+
+get '/all' do
+  @teamcategory = Teamparse.new.teamcategory.upcase
+  @items = Teamparse.new.allitems.map.each_with_index do |item, index|
+    @itemcontent = item.xpath('//content:encoded', 'content' => "http://purl.org/rss/1.0/modules/content/").text #.map {|itemcontent| itemcontent.text}
+    {
+      itemtitle: item.css('title').text, #.map { |title| title.text }
+      itemurl: item.css('link').text, #.map { |link| link.text }
+      itemid: item.css('guid').text, #.map { |link| link.text }
+      itemframe: Nokogiri::XML(@itemcontent).css('iframe').map {|itemframe| itemframe.attr('src')}[index] #.map { |item| item }}
+    }
+    end
+
+  erb :all
+end
+
+get '/hr' do
   @teamcategory = Teamparse.new.teamcategory.upcase
   @items = Teamparse.new.items.map.each_with_index do |item, index|
     @itemcontent = item.xpath('//content:encoded', 'content' => "http://purl.org/rss/1.0/modules/content/").text #.map {|itemcontent| itemcontent.text}
@@ -44,6 +76,13 @@ get '/' do
     }
     end
 
-  erb :default
-
+  erb :hr
 end
+
+post '/' do
+  pick_team = params[:pick_team]
+
+  @your_choice = YourChoice.new(pick_team)
+  redirect '/to_a_page'
+end
+
